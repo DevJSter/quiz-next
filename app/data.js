4fa97b37f30dd07e407ca7dd13f6ce0a7976f767
Shubham Tiwari <shubht3303@gmail.com>
2024-03-16 18:47:12 +0530

    riiiii

--- app/data.js
+++ app/data.js
@@ -1,133 +1,379 @@
 export const quiz = {
   totalQuestions: 14,
+  category: {
+    aspiration: 0,
+    financialFitness: 0,
+    energyManagement: 0,
+    precision: 0,
+    leadership: 0,
+    connection: 0,
+    selfExpertise: 0,
+  },
   questions: [
     {
       id: 1,
-      question: "Which statements corresponds to your financial fitness??",
+      category: "aspiration",
+      question: "Which statements corresponds to your financial fitness?",
       answers: [
-        "I let financial stress affect me daily",
-        "I often worry about my present and future finances",
-        "I struggle with confidence quite often",
+        {
+          answer: "I let financial stress affect me daily",
+          updateCategory: () => {
+            quiz.category.financialFitness--;
+          },
+        },
+        {
+          answer: "I often worry about my present and future finances",
+          updateCategory: () => {
+            quiz.category.financialFitness++;
+          },
+        },
+        {
+          answer: "I am always on top of  my game when it comes to financial planning",
+          updateCategory: () => {
+            quiz.category.financialFitness++;
+          },
+        },
       ],
     },
     {
       id: 2,
+      category: "aspiration",
       question: "Which statement best corresponds to your level of confidence?",
       answers: [
-        "I am a confident person",
-        "I am always nervous and unsure of myself",
-        "I struggle with unconfidence quite often",
+        {
+          answer: "I am a confident person",
+          updateCategory: () => {
+            quiz.category.aspiration++;
+          },
+        },
+        {
+          answer: "I am always nervous and unsure of myself",
+          updateCategory: () => {
+            quiz.category.aspiration--;
+          },
+        },
+        {
+          answer: "I struggle with unconfidence quite often",
+          updateCategory: () => {
+            quiz.category.aspiration--;
+          },
+        },
       ],
     },
     {
       id: 3,
+      category: "aspiration",
       question: "Which Statement best corresponds to your levels of empathy?",
       answers: [
-        "I can easily relate to people's struggles",
-        "Empathising with others is a easy task to me",
-        "I can be empathetic to some but not to all",
+        {
+          answer: "I can easily relate to people's struggles",
+          updateCategory: () => {
+            quiz.category.connection++;
+          },
+        },
+        {
+          answer: "Empathising with others is an easy task for me",
+          updateCategory: () => {
+            quiz.category.connection++;
+          },
+        },
+        {
+          answer: "I can be empathetic to some but not to all",
+          updateCategory: () => {
+            quiz.category.aspiration--;
+          },
+        },
       ],
     },
     {
       id: 4,
-      question: "Which statements best corresponds to your ability of focus?",
+      category: "aspiration",
+      question: "Which statements best correspond to your ability to focus?",
       answers: [
-        "Once I am focused no one can move me.",
-        "I am aware of some distractions",
-        "I can never win over my distractions",
+        {
+          answer: "Once I am focused no one can move me.",
+          updateCategory: () => {
+            quiz.category.leadership++;
+          },
+        },
+        {
+          answer: "I am aware of some distractions",
+          updateCategory: () => {
+            quiz.category.energyManagement++;
+          },
+        },
+        {
+          answer: "I can never win over my distractions",
+          updateCategory: () => {
+            quiz.category.leadership--;
+          },
+        },
       ],
     },
     {
       id: 5,
+      category: "financialFitness",
       question:
         "Which statement best corresponds to your ability to maintain boundaries?",
       answers: [
-        "I have a clear picture of what boundaries: physical and emotional look alike",
-        "I feel people do not respect my boundaries",
-        "I can maintain boundaries with some but struggle with others",
+        {
+          answer:
+            "I have a clear picture of what boundaries: physical and emotional look alike",
+          updateCategory: () => {
+            quiz.category.financialFitness++;
+          },
+        },
+        {
+          answer: "I feel people do not respect my boundaries",
+          updateCategory: () => {
+            quiz.category.financialFitness++;
+          },
+        },
+        {
+          answer:
+            "I can maintain boundaries with some but struggle with others",
+          updateCategory: () => {
+            quiz.category.financialFitness++;
+          },
+        },
       ],
     },
     {
       id: 6,
+      category: "financialFitness",
       question: "Which statement best corresponds to your career goals?",
       answers: [
-        "I know about my goals are and I am actively working towards them",
-        "It's getting slightly difficult to have a consistent version of what goals I want to achieve.",
-        "I struggle a lot with what I want to do and how I want to achieve it",
+        {
+          answer:
+            "I know what my goals are and I am actively working towards them",
+          updateCategory: () => {
+            quiz.category.aspiration++;
+          },
+        },
+        {
+          answer:
+            "It's getting slightly difficult to have a consistent version of what goals I want to achieve.",
+          updateCategory: () => {
+            quiz.category.aspiration--;
+          },
+        },
+        {
+          answer:
+            "I struggle a lot with what I want to do and how I want to achieve it",
+          updateCategory: () => {
+            quiz.category.selfExpertise++;
+          },
+        },
       ],
     },
     {
       id: 7,
+      category: "energyManagement",
       question: "Which statement best corresponds to your energy levels?",
       answers: [
-        "I am energized to tackle my day",
-        "I can get tired easily",
-        "I don't know why I'm always exhausted",
+        {
+          answer: "I am energized to tackle my day",
+          updateCategory: () => {
+            quiz.category.leadership++;
+          },
+        },
+        {
+          answer: "I can get tired easily",
+          updateCategory: () => {
+            quiz.category.energyManagement++;
+          },
+        },
+        {
+          answer: "I don't know why I'm always exhausted",
+          updateCategory: () => {
+            quiz.category.energyManagement--;
+          },
+        },
       ],
     },
     {
       id: 8,
+      category: "energyManagement",
       question: "Which statement best corresponds to your spending habits?",
       answers: [
-        "I worry about every single purchase",
-        "I prioritize bills and savings and spend on an as-needed basis",
-        "I am impulsive in my spending and rarely keep track of it",
+        {
+          answer: "I worry about every single purchase",
+          updateCategory: () => {
+            quiz.category.financialFitness--;
+          },
+        },
+        {
+          answer:
+            "I prioritize bills and savings and spend on an as-needed basis",
+          updateCategory: () => {
+            quiz.category.financialFitness++;
+          },
+        },
+        {
+          answer: "I am impulsive in my spending and rarely keep track of it",
+          updateCategory: () => {
+            quiz.category.energyManagement++;
+            quiz.category.leadership++;
+          },
+        },
       ],
     },
     {
       id: 9,
+      category: "precision",
       question:
         "Which statement best corresponds to your perspective on challenges?",
       answers: [
-        "It's hard to see the opportunity when I am challenged",
-        "I seize the opportunity in the challenges ahead",
-        "My life obstacles overwhelm me",
+        {
+          answer: "It's hard to see the opportunity when I am challenged",
+          updateCategory: () => {
+            quiz.category.precision--;
+            
+          },
+        },
+        {
+          answer: "I seize the opportunity in the challenges ahead",
+          updateCategory: () => {
+            quiz.category.precision++;
+            quiz.category.leadership++;
+          },
+        },
+        {
+          answer: "My life obstacles overwhelm me",
+          updateCategory: () => {
+            quiz.category.precision++;
+          },
+        },
       ],
     },
     {
       id: 10,
+      category: "precision",
       question:
         "Which statements best corresponds to your personal and professional relationships?",
       answers: [
-        "I feel safe being myself in all of my relationships",
-        "I am comfortable expressing my opinions and thoughts to others",
-        "I often hesitate in expressing my honest thoughts and feelings in some of my relationships",
+        {
+          answer: "I feel safe being myself in all of my relationships",
+          updateCategory: () => {
+            quiz.category.precision++;
+          },
+        },
+        {
+          answer:
+            "I am comfortable expressing my opinions and thoughts to others",
+          updateCategory: () => {
+            quiz.category.precision++;
+          },
+        },
+        {
+          answer:
+            "I often hesitate in expressing my honest thoughts and feelings in some of my relationships",
+          updateCategory: () => {
+            quiz.category.precision++;
+          },
+        },
       ],
     },
     {
       id: 11,
+      category: "leadership",
       question: "Which statements best corresponds to your level of optimism?",
       answers: [
-        "I consider myself an optimistic person",
-        "I feel I am consistently jumping to negative thoughts",
-        "I try to be optimistic but it's hard to not drift towards the negatives",
+        {
+          answer: "I consider myself an optimistic person",
+          updateCategory: () => {
+            quiz.category.leadership++;
+          },
+        },
+        {
+          answer: "I feel I am consistently jumping to negative thoughts",
+          updateCategory: () => {
+            quiz.category.leadership++;
+          },
+        },
+        {
+          answer:
+            "I try to be optimistic but it's hard to not drift towards the negatives",
+          updateCategory: () => {
+            quiz.category.leadership++;
+          },
+        },
       ],
     },
     {
       id: 12,
+      category: "leadership",
       question: "Which statement best corresponds to your trustworthiness?",
       answers: [
-        "I feel everyone in my life easily trusts me",
-        "It takes effort to gain others' trust",
-        "Despite my best efforts, people don't trust me",
+        {
+          answer: "I feel everyone in my life easily trusts me",
+          updateCategory: () => {
+            quiz.category.leadership++;
+          },
+        },
+        {
+          answer: "It takes effort to gain others' trust",
+          updateCategory: () => {
+            quiz.category.leadership++;
+          },
+        },
+        {
+          answer: "Despite my best efforts, people don't trust me",
+          updateCategory: () => {
+            quiz.category.leadership++;
+          },
+        },
       ],
     },
     {
       id: 13,
+      category: "connection",
       question: "Which statement best corresponds to your sleep hygiene?",
       answers: [
-        "My sleep pattern keeps changing and it's irregular",
-        "I have uncontrollable urges to fall asleep during the day",
-        "I am mindful about the quality and quantity of my sleep",
+        {
+          answer: "My sleep pattern keeps changing and it's irregular",
+          updateCategory: () => {
+            quiz.category.connection++;
+          },
+        },
+        {
+          answer: "I have uncontrollable urges to fall asleep during the day",
+          updateCategory: () => {
+            quiz.category.connection++;
+          },
+        },
+        {
+          answer: "I am mindful about the quality and quantity of my sleep",
+          updateCategory: () => {
+            quiz.category.connection++;
+          },
+        },
       ],
     },
     {
       id: 14,
-      question: "Which statement best corresponds to your trustworthiness?",
+      category: "connection",
+      question:
+        "Which statement best corresponds to your ability to stay calm?",
       answers: [
-        "I am constantly a bundle of nerves",
-        "I sometimes struggle to calm myself down",
-        "I don't let situations disturb my calm",
+        {
+          answer: "I am constantly a bundle of nerves",
+          updateCategory: () => {
+            quiz.category.connection++;
+          },
+        },
+        {
+          answer: "I sometimes struggle to calm myself down",
+          updateCategory: () => {
+            quiz.category.connection++;
+          },
+        },
+        {
+          answer: "I don't let situations disturb my calm",
+          updateCategory: () => {
+            quiz.category.connection++;
+          },
+        },
       ],
     },
   ],
--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -6,62 +6,15 @@ const Page = () => {
   const [activeQuestion, setActiveQuestion] = useState(0);
   const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [showResult, setShowResult] = useState(false);
-  const [result, setResult] = useState({
-    aspiration: 0,
-    financialFitness: 0,
-    energyManagement: 0,
-    precision: 0,
-    leadership: 0,
-    connection: 0,
-    selfExpertise: 0,
-  });
+  const [result, setResult] = useState(null);
 
   const { questions } = quiz;
-  const { question, answers, correctAnswer } = questions[activeQuestion];
+  const currentQuestion = questions[activeQuestion];
 
   // Select answer and update score
   const onAnswerSelected = (answer, idx) => {
     setSelectedAnswerIndex(idx);
-    const property = getPropertyForQuestion(activeQuestion);
-    const scoreIncrement = calculateScoreIncrement(answer, correctAnswer);
-    setResult((prev) => ({
-      ...prev,
-      [property]: prev[property] + scoreIncrement,
-    }));
-  };
-
-  // Calculate score increment based on answer correctness
-  const calculateScoreIncrement = (selectedAnswer, correctAnswer) => {
-    return selectedAnswer === correctAnswer ? 1 : 0;
-  };
-
-  // Determine which property the current question corresponds to
-  const getPropertyForQuestion = (questionIndex) => {
-    switch (questionIndex) {
-      case 0:
-      case 1:
-        return "aspiration";
-      case 2:
-      case 3:
-        return "financialFitness";
-      case 4:
-      case 5:
-        return "energyManagement";
-      case 6:
-      case 7:
-        return "precision";
-      case 8:
-      case 9:
-        return "leadership";
-      case 10:
-      case 11:
-        return "connection";
-      case 12:
-      case 13:
-        return "selfExpertise";
-      default:
-        return "";
-    }
+    answer.updateCategory(); // Increment category count
   };
 
   // Proceed to the next question or show result
@@ -69,9 +22,24 @@ const Page = () => {
     if (activeQuestion < questions.length - 1) {
       setActiveQuestion(activeQuestion + 1);
     } else {
+      calculateResult();
       setShowResult(true);
     }
     setSelectedAnswerIndex(null);
+  };
+
+  // Calculate final result based on category scores
+  const calculateResult = () => {
+    const { category } = quiz;
+    const totalQuestions = questions.length;
+    const updatedResult = {};
+
+    for (const categoryName in category) {
+      const categoryScore = (category[categoryName] / totalQuestions) * 100;
+      updatedResult[categoryName] = categoryScore;
+    }
+
+    setResult(updatedResult);
   };
 
   return (
@@ -86,18 +54,20 @@ const Page = () => {
       <div>
         {!showResult ? (
           <div className="quiz-container">
-            <h3>{question}</h3>
-            {answers.map((answer, idx) => (
-              <li
-                key={idx}
-                onClick={() => onAnswerSelected(answer, idx)}
-                className={
-                  selectedAnswerIndex === idx ? "li-selected" : "li-hover"
-                }
-              >
-                <span>{answer}</span>
-              </li>
-            ))}
+            <h3>{currentQuestion.question}</h3>
+            <ul>
+              {currentQuestion.answers.map((answer, idx) => (
+                <li
+                  key={idx}
+                  onClick={() => onAnswerSelected(answer, idx)}
+                  className={
+                    selectedAnswerIndex === idx ? "li-selected" : "li-hover"
+                  }
+                >
+                  <span>{answer.answer}</span>
+                </li>
+              ))}
+            </ul>
             <button onClick={nextQuestion} className="btn">
               {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
             </button>
@@ -105,13 +75,12 @@ const Page = () => {
         ) : (
           <div className="quiz-container">
             <h3>Results</h3>
-            <p>Aspiration: {result.aspiration * 25}%</p>
-            <p>Financial Fitness: {result.financialFitness * 25}%</p>
-            <p>Energy Management: {result.energyManagement * 25}%</p>
-            <p>Precision: {result.precision * 25}%</p>
-            <p>Leadership: {result.leadership * 25}%</p>
-            <p>Connection: {result.connection * 25}%</p>
-            <p>Self Expertise: {result.selfExpertise * 25}%</p>
+            {result &&
+              Object.entries(result).map(([category, score]) => (
+                <p key={category}>
+                  {category}: {score}%
+                </p>
+              ))}
             <button onClick={() => window.location.reload()}>Restart</button>
           </div>
         )}
